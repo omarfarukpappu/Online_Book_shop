307ae1132307d76c78307b76005c8205ce73fff6
omarfarukpappu <omarfarukpappu666@gmail.com>
2023-11-08 16:41:52 +0600

    update

--- insert.php
+++ insert.php
@@ -14,38 +14,55 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $confirmPassword = mysqli_real_escape_string($conn, $_POST['confirm_password']);
     $user_type = $_POST['user_type'];
 
-  
-    if (!validatePassword($password)) {
-        $_SESSION['message'][] = 'Password must be at least 6 characters long and contain at least one special character.';
+    // File upload handling
+    $uploadDir = 'image_photo/'; // Directory where the files will be saved
+    $uploadFile = $uploadDir . basename($_FILES['uploaded_img']['name']);
+    $imageFileType = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
+
+    // Check if the file is an image
+    $check = getimagesize($_FILES['uploaded_img']['tmp_name']);
+    if ($check === false) {
+        $_SESSION['message'][] = 'File is not an image.';
+    } elseif (file_exists($uploadFile)) {
+        $_SESSION['message'][] = 'File already exists.';
+    } elseif ($_FILES['uploaded_img']['size'] > 50000000) {
+        $_SESSION['message'][] = 'File is too large (greater than 5MB).';
+    } elseif (!in_array($imageFileType, ['jpg', 'jpeg', 'png'])) {
+        $_SESSION['message'][] = 'Only JPG, JPEG, and PNG files are allowed.';
     } else {
-    
-        $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
+        move_uploaded_file($_FILES['uploaded_img']['tmp_name'], $uploadFile);
 
-        if (mysqli_num_rows($select_users) > 0) {
-            $_SESSION['message'][] = 'User already exists!';
+        if (!validatePassword($password)) {
+            $_SESSION['message'][] = 'Password must be at least 6 characters long and contain at least one special character.';
         } else {
-      
-            if ($password != $confirmPassword) {
-                $_SESSION['message'][] = 'Confirm password not matched!';
+            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
+
+            if (mysqli_num_rows($select_users) > 0) {
+                $_SESSION['message'][] = 'User already exists!';
             } else {
-           
-                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-                
-                $insertQuery = "INSERT INTO `users` (name, email, password, user_type) VALUES (?, ?, ?, ?)";
-                $stmt = mysqli_prepare($conn, $insertQuery);
-
-                mysqli_stmt_bind_param($stmt, 'ssss', $name, $email, $hashedPassword, $user_type);
-
-                if (mysqli_stmt_execute($stmt)) {
-                    $_SESSION['message'][] = 'Registered successfully!';
-                    header('location: display_users.php');
-                    exit();
+                if ($password != $confirmPassword) {
+                    $_SESSION['message'][] = 'Confirm password not matched!';
                 } else {
-                    $_SESSION['message'][] = 'Error inserting data: ' . mysqli_error($conn);
-                }
+                    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-                mysqli_stmt_close($stmt);
+                    $insertQuery = "INSERT INTO `users` (name, email, password, user_type, profile_pic) VALUES (?, ?, ?, ?, ?)";
+                    $stmt = mysqli_prepare($conn, $insertQuery);
+
+
+
+                    // Bind parameters including the profile picture path
+                    mysqli_stmt_bind_param($stmt, 'sssss', $name, $email, $hashedPassword, $user_type, $uploadFile);
+
+                    if (mysqli_stmt_execute($stmt)) {
+                        $_SESSION['message'][] = 'Registered successfully!';
+                        header('location: display_users.php');
+                        exit();
+                    } else {
+                        $_SESSION['message'][] = 'Error inserting data: ' . mysqli_error($conn);
+                    }
+
+                    mysqli_stmt_close($stmt);
+                }
             }
         }
     }
@@ -59,7 +76,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header('location: ' . $_SERVER['PHP_SELF']);
     exit();
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -111,6 +127,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             box-sizing: border-box;
         }
 
+        input[type="file"] {
+            margin-bottom: 20px;
+        }
+
         input[type="submit"] {
             background-color: #4CAF50;
             color: white;
@@ -126,7 +146,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             margin-bottom: 20px;
         }
     </style>
-
 </head>
 <body>
 
@@ -138,18 +157,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         foreach ($_SESSION['message'] as $message) {
             echo '<p class="error-message">' . $message . '</p>';
         }
-     
+
         unset($_SESSION['message']);
     }
     ?>
 
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <label for="name">Name:</label>
         <input type="text" name="name" value="<?php echo htmlspecialchars(isset($_SESSION['formData']['name']) ? $_SESSION['formData']['name'] : ''); ?>" required>
 
         <label for="email">Email:</label>
-        <input type="email" name="email"
-               value="<?php echo htmlspecialchars(isset($_SESSION['formData']['email']) ? $_SESSION['formData']['email'] : ''); ?>" required>
+        <input type="email" name="email" value="<?php echo htmlspecialchars(isset($_SESSION['formData']['email']) ? $_SESSION['formData']['email'] : ''); ?>" required>
 
         <label for="password">Password:</label>
         <input type="password" name="password" required>
@@ -159,15 +177,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <label for="user_type">User Type:</label>
         <select name="user_type">
-            <option value="user"
-                <?php echo (isset($_SESSION['formData']['user_type']) && $_SESSION['formData']['user_type'] === 'user') ? 'selected' : ''; ?>>
+            <option value="user" <?php echo (isset($_SESSION['formData']['user_type']) && $_SESSION['formData']['user_type'] === 'user') ? 'selected' : ''; ?>>
                 User
             </option>
-            <option value="admin"
-                <?php echo (isset($_SESSION['formData']['user_type']) && $_SESSION['formData']['user_type'] === 'admin') ? 'selected' : ''; ?>>
+            <option value="admin" <?php echo (isset($_SESSION['formData']['user_type']) && $_SESSION['formData']['user_type'] === 'admin') ? 'selected' : ''; ?>>
                 Admin
             </option>
         </select>
+
+        <label for="uploaded_img">Profile Picture:</label>
+        <input type="file" name="uploaded_img" accept="image/jpg, image/jpeg, image/png">
 
         <input type="submit" value="Insert">
     </form>
@@ -177,6 +196,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </html>
 
 <?php
-
 unset($_SESSION['formData']);
 ?>
